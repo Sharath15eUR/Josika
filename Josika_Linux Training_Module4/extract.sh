646980138728e45e66ef31fa7a21a82d0f5d8d57
Josika <140434196+JosikaThiyagarajan@users.noreply.github.com>
2025-02-02 23:15:19 +0530

    Updated extract.sh

--- Josika_Linux Training_Module4/extract.sh
+++ Josika_Linux Training_Module4/extract.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#checking if the input file is provided
+
+# Checking if the input file is provided
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <input_file>"
     exit 1
@@ -7,6 +8,12 @@ fi
 
 input="$1"
 output="output.txt"
+
+# Check if input file exists
+if [ ! -f "$input" ]; then
+    echo "Error: File '$input' not found!"
+    exit 1
+fi
 
 # Clear the output file before writing
 > "$output"
@@ -16,24 +23,25 @@ frame_time=""
 wlan_type=""
 wlan_sub_type=""
 
+# Read input file line by line
 while IFS= read -r line; do
     case "$line" in
         *"frame.time"*)
-            [[ "$line" != *"frame.time_"* ]] && frame_time=$(echo "$line" | awk -F': ' '{print $2}' | tr -d ',')
+            [[ "$line" != *"frame.time_"* ]] && frame_time=$(echo "$line" | cut -d':' -f2- | xargs)
             ;;
         *"wlan.fc.type"*)
-            [[ "$line" != *"wlan.fc.type_"* ]] && wlan_type=$(echo "$line" | awk -F': ' '{print $2}' | tr -d ',')
+            [[ "$line" != *"wlan.fc.type_"* ]] && wlan_type=$(echo "$line" | cut -d':' -f2- | xargs)
             ;;
         *"wlan.fc.subtype"*)
-            [[ "$line" != *"wlan.fc.subtype_"* ]] && wlan_sub_type=$(echo "$line" | awk -F': ' '{print $2}' | tr -d ',')
+            [[ "$line" != *"wlan.fc.subtype_"* ]] && wlan_sub_type=$(echo "$line" | cut -d':' -f2- | xargs)
             ;;
     esac
 
-    # If all values are found, write to output and reset
+    # If all values are found, write to output with proper formatting and reset
     if [[ -n "$frame_time" && -n "$wlan_type" && -n "$wlan_sub_type" ]]; then
-        echo "\"frame.time\": \"$frame_time\"" >> "$output"
-        echo "\"wlan.fc.type\": \"$wlan_type\"" >> "$output"
-        echo "\"wlan.fc.subtype\": \"$wlan_sub_type\"" >> "$output"
+        echo "\"frame.time\": \"$frame_time\"," >> "$output"
+        echo "\"wlan.fc.type\": \"$wlan_type\"," >> "$output"
+        echo "\"wlan.fc.subtype\": \"$wlan_sub_type\"," >> "$output"
         echo "" >> "$output"  # Add a blank line for readability
         frame_time=""; wlan_type=""; wlan_sub_type=""
     fi
